4050673a20c8fb7a34870ee0fdc69dcb8220288d
a-Osama <aosama.dev@gmail.com>
2021-07-29 06:43:11 +0200

    JSON Serializer

--- shortner/model.go
+++ shortner/model.go
@@ -2,7 +2,7 @@ package shotner
 
 // Redirect Model Containing annotations for JSON, MongoDB, msgPack and Validator
 type Redirect struct {
-	Code      string `json:"code" bson:"code" msgpack:"code"`
-	URL       string `json:"url" bson:"url" msgpack:"url" validate:"empty=false & format=url"`
-	CreatedAt int64  `json:"createdAt" bson:"createdAt" msgpack:"createdAt"`
+	Code      string `json:"code" bson:"code"`
+	URL       string `json:"url" bson:"url" validate:"empty=false & format=url"`
+	CreatedAt int64  `json:"createdAt" bson:"createdAt"`
 }
